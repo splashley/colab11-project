f60fbdaf23b8a19c755e6e4fec93f6b83c9461e9
splashley <ashleyjlhynes@gmail.com>
2022-06-05 23:22:48 -0400

    added popup modal for feedback

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Home from "./routes/Home";
 import ScanJobDescription from "./routes/ScanJobDescription";
 
 import { Route, Routes, Outlet } from "react-router-dom";
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 function App() {
   return (
@@ -16,52 +16,55 @@ function App() {
         </div>
         <div>
           <Popup
-    trigger={<button className="btn btn--blue">Feedback</button>}
-    modal
-    nested
-  >
-    {close => (
-      <div className="modal">
-        <button className="close" onClick={close}>
-          &times;
-        </button>
-        <div className="header"> Modal Title </div>
-        <div className="content">
-          {' '}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-          Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-          delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-          commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
-          explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
-        </div>
-        <div className="actions">
-          <Popup
-            trigger={<button className="button"> Trigger </button>}
-            position="top center"
+            trigger={<button className="btn btn--blue">Feedback</button>}
+            modal
             nested
           >
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
-              magni omnis delectus nemo, maxime molestiae dolorem numquam
-              mollitia, voluptate ea, accusamus excepturi deleniti ratione
-              sapiente! Laudantium, aperiam doloribus. Odit, aut.
-            </span>
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Modal Title </div>
+                <div className="content">
+                  {" "}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel
+                  eveniet quibusdam voluptates delectus doloremque, explicabo
+                  tempore dicta adipisci fugit amet dignissimos?
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur sit commodi beatae optio voluptatum sed eius
+                  cumque, delectus saepe repudiandae explicabo nemo nam libero
+                  ad, doloribus, voluptas rem alias. Vitae?
+                </div>
+                <div className="actions">
+                  <Popup
+                    trigger={<button className="button"> Trigger </button>}
+                    position="top center"
+                    nested
+                  >
+                    <span>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Beatae magni omnis delectus nemo, maxime molestiae dolorem
+                      numquam mollitia, voluptate ea, accusamus excepturi
+                      deleniti ratione sapiente! Laudantium, aperiam doloribus.
+                      Odit, aut.
+                    </span>
+                  </Popup>
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log("modal closed ");
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
           </Popup>
-          <button
-            className="button"
-            onClick={() => {
-              console.log('modal closed ');
-              close();
-            }}
-          >
-            close modal
-          </button>
-        </div>
-      </div>
-    )}
-  </Popup>
         </div>
       </header>
       <Outlet />
