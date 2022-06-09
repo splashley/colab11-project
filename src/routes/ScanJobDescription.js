6f419699a2823ac715b560b547e28467ba3125b3
splashley <ashleyjlhynes@gmail.com>
2022-06-09 08:37:00 -0400

    fixed responsiveness on scanjobdesc page

--- src/routes/ScanJobDescription.js
+++ src/routes/ScanJobDescription.js
@@ -2,12 +2,11 @@ import * as React from 'react';
 
 export default function ScanJobDescription() {
   return (
-    <main style={{textAlign: "center"}}>
+    <main className="scan-job-description-page" style={{textAlign: "center"}}>
       <h2>Let’s get started...paste your job description below to start the keywords scan. </h2>
       <p>Exclude the “About the company” section</p>
-      <form>
-      <textarea>
-  Content of the textarea.
+      <form className="scan-job-description-form">
+      <textarea placeholder="Paste Your Job Description Here">
 </textarea><br />
 <button className="btn btn--blue">Submit</button>
     </form>
